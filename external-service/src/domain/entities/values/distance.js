98f67ebd1b97422013c4e48c60531b68b1e69715
Abner Tellez <atellezsazo@gmail.com>
2019-07-20 22:07:57 +0800

    Implement GeoLib
    Implement GeoLib for distance calculation.

--- external-service/src/domain/entities/values/distance.js
+++ external-service/src/domain/entities/values/distance.js
@@ -1,6 +1,7 @@
 const isValidCoordinates = require('is-valid-coordinates');
+const geolib = require('geolib');
 
-const calcDistance = (coordinatesFrom, coordinatesTo, unit = 'K') => {
+const calcDistance = (coordinatesFrom, coordinatesTo) => {
   if ((coordinatesFrom.latitude === coordinatesTo.latitude
     && coordinatesFrom.longitude === coordinatesTo.longitude)
     || (!isValidCoordinates(coordinatesFrom.longitude, coordinatesFrom.latitude)
@@ -8,21 +9,12 @@ const calcDistance = (coordinatesFrom, coordinatesTo, unit = 'K') => {
     return 0;
   }
 
-  const radLat1 = Math.PI * coordinatesFrom.latitude / 180;
-  const radLat2 = Math.PI * coordinatesTo.latitude / 180;
-  const theta = coordinatesFrom.longitude - coordinatesTo.longitude;
-  const radTheta = Math.PI * theta / 180;
-  let dist = Math.sin(radLat1) * Math.sin(radLat2) + Math.cos(radLat1)
-    * Math.cos(radLat2) * Math.cos(radTheta);
-  if (dist > 1) {
-    dist = 1;
-  }
-  dist = Math.acos(dist);
-  dist = dist * 180 / Math.PI;
-  dist = dist * 60 * 1.1515;
-  if (unit === 'K') { dist *= 1.609344; }
-  if (unit === 'N') { dist *= 0.8684; }
-  return dist;
+  const distance = geolib.getDistance(
+    coordinatesFrom,
+    coordinatesTo,
+  );
+
+  return distance / 1000;
 };
 
 module.exports = {
